a607378585399153c5e82c66c15a64cd0c806552
Ubuntu <ubuntu@ip-172-31-9-28.ec2.internal>
2021-06-21 20:28:35 +0000

    Login and Registration

---
+++ incorrect_password_check.php
@@ -0,0 +1,13 @@
+<?php
+$password = "test";
+echo "Password: $password <br>";
+$hash = password_hash($password, PASSWORD_BCRYPT);
+$hash_compare = password_hash($password, PASSWORD_BCRYPT);
+if($hash == $hash_compare){
+  echo "Hashes match!";
+}
+else{
+  echo "Hashes don't match";
+}
+echo "<br>Hash1: $hash<br>Hash2:$hash_compare<br>";
+?>
